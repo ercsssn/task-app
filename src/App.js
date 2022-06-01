04916087434ee4cd500cdcf028024ec2076b5f26
John Ericsson <ercsssn@gmail.com>
2022-06-01 13:26:55 +0800

    Create the component skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: {text:''},
+      tasks: [],
+    };
+  }
+
+  render() {
+    const {task, tasks} = this.state;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="taskInput">Enter task</label>
+          <input type="text" id="taskInput"/>
+          <button type="submit">
+            Add task
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
